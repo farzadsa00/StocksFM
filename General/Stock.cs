1c0153fbf2c88064af114351e4d3c40a25b02a2b
Farzad <farzad.kh72@gmail.com>
2020-10-14 20:02:54 +0330

    Stock class added

---
+++ General/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksFM.General
+{
+    class Stock
+    {
+        public string Name { get; }
+        public char StockType { get; set; }
+        public char SubStockType { get; }
+        public int CurrentPrice { get; set; }
+        public int FinalPrice { get; set; }
+        public string Status { get; set; }
+    }
+}
